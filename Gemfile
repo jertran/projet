decd687bb0b65a2d6c98172bb1ff0287c767a826
jeremy.tran <jeremy.tran@utbm.fr>
2018-03-17 15:20:46 -0400

    adding simple cov formatter

--- Gemfile
+++ Gemfile
@@ -53,3 +53,6 @@ gem 'rspec-core', '3.7.1'
 gem 'rspec-expectations', '3.7.0'
 gem 'rspec-mocks', '3.7.0'
 gem 'rspec-support', '3.7.1'
+
+# Use forecast.io
+gem 'forecast_io'
--- app/models/town.rb
+++ app/models/town.rb
@@ -1,6 +1,9 @@
 class Town < ActiveRecord::Base
   before_validation :geocode_address
   validates :name, :latitude, :longitude, presence: true
+  
+  @@requested_weather
+  
   private
     def geocode_address
       place = Nominatim.search(self.name).limit(1).address_details(true)
@@ -10,5 +13,14 @@ class Town < ActiveRecord::Base
         self.longitude = current_town.lon
       end
     end
-
+  private
+    def get_wheather
+      wheather = ForecastIO::Forecast.new.coordinates(latitude: self.latitude, longitude: self.latitude)
+      if wheather
+        @@requested_weather = wheather
+      end
+    end
+  def self.weather
+    @@requested_weather
+  end
 end
